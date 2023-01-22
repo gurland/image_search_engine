d572d6dd5ee4121c5d87c6a21ea2399884b4c774
gurland <stas.gurland@gmail.com>
2023-01-22 21:54:33 +0200

    fix: initital commit

--- image_indexer_service/src/main.py
+++ image_indexer_service/src/main.py
@@ -11,21 +11,22 @@ from io import BytesIO
 from time import sleep
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
+import uuid
 
 URL = "http://olidawiki.s3-website.eu-central-1.amazonaws.com/c676eba0-78ee-42f4-a48c-758cdb77a5f8.jpg"
 
-model = ResNet50(weights='imagenet', include_top=False,
+MODEL = ResNet50(weights='imagenet', include_top=False,
                  input_shape=(224, 224, 3), pooling='avg')
 
 
-def extract_features(img_url, model):
+def extract_features(img_url):
     r = requests.get(img_url)
     img = PIL.Image.open(BytesIO(r.content)).resize((224, 224)).convert("RGB")
 
     img_array = image.img_to_array(img)
     expanded_img_array = np.expand_dims(img_array, axis=0)
     preprocessed_img = preprocess_input(expanded_img_array)
-    features = model.predict(preprocessed_img)
+    features = MODEL.predict(preprocessed_img)
     flattened_features = features.flatten()
     normalized_features = flattened_features / norm(flattened_features)
     return normalized_features
@@ -39,10 +40,34 @@ QDRANT_HOST = os.getenv("QDRANT_HOST", "http://images_api:80/api")
 
 client = QdrantClient(host=QDRANT_HOST, port=6333)
 
-client.recreate_collection(
-    collection_name="images_collection",
-    vectors_config=models.VectorParams(size=2048, distance=models.Distance.COSINE),
-)
+
+try:
+    client.get_collection(collection_name="images_collection")
+except Exception as e:
+    client.recreate_collection(
+        collection_name="images_collection",
+        vectors_config=models.VectorParams(size=2048, distance=models.Distance.COSINE),
+    )
+
+
+def add_image_to_index(image_url):
+    features = extract_features(image_url)
+
+    try:
+        point_id = image_url.split("/")[-1].split(".jpg")[0]
+    except:
+        point_id = str(uuid.uuid4())
+
+    client.upsert(
+        collection_name="images_collection",
+        points=[
+            models.PointStruct(
+                id=point_id,
+                vector=features.tolist(),
+            ),
+        ]
+    )
+
 
 if __name__ == '__main__':
     while True:
